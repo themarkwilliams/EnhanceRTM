3d4bafa134e64a47e3f1f705ed100dcd20df065c
themarkwilliams <themarkwilliams@users.noreply.github.com>
2016-03-29 09:47:13 -0500

    Forgot to update version number

--- userscript.js
+++ userscript.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name EnhanceRTM
-// @version 1.0
+// @version 1.1
 // @author Mark Williams
 // @description "Get rid of the metallic pea highlight color"
 // @match        https://www.rememberthemilk.com/*
