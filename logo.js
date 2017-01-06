a962dc83d8ac753863023fd2dd6d85265bcde31b
themarkwilliams <themarkwilliams@users.noreply.github.com>
2017-01-06 08:10:47 -0600

    Added correct commenting

--- logo.js
+++ logo.js
@@ -9,7 +9,8 @@
 // ==/UserScript==
 // Git: https://github.com/themarkwilliams/EnhanceRTM/blob/master/logo.js
 
-// Set tag color in task list
+// Hide the Logo
 GM_addStyle('.b-rn-qo { display:none !important; }'); // 1/6/17
 
-GM_addStyle('.b-Ep-Ej-Fp-Yn { top:0 !important; bottom:0 !important; height:100% !important; }'); //1/6/17
+// Adjust the height of the left navigation to compensate for the missing logo
+GM_addStyle('.b-Ep-Ej-Fp-Yn { top:0 !important; bottom:0 !important; height:100% !important; }'); // 1/6/17
