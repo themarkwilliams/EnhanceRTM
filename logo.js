1531f5400e0e4b52d7c0f819726d713376fcd103
themarkwilliams <themarkwilliams@users.noreply.github.com>
2017-01-06 08:41:24 -0600

    Add code to change favicon

--- logo.js
+++ logo.js
@@ -14,3 +14,14 @@ GM_addStyle('.b-rn-qo { display:none !important; }'); // 1/6/17
 
 // Adjust the height of the left navigation to compensate for the missing logo
 GM_addStyle('.b-Ep-Ej-Fp-Yn { top:0 !important; bottom:0 !important; height:100% !important; }'); // 1/6/17
+
+// Change the Favicon
+var favicon_link_html = document.querySelector("link[rel*='icon']") || document.createElement('link');
+favicon_link_html.rel = 'shortcut icon';
+favicon_link_html.href = 'http://marktastic.com/images/rtmfavicon.ico';
+favicon_link_html.type = 'image/x-icon';
+
+try { 
+  document.getElementsByTagName('head')[0].appendChild( favicon_link_html ); 
+}
+catch(e) { }
