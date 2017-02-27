651845a491e879e58f8a1d400cf685c79aa77969
themarkwilliams <themarkwilliams@users.noreply.github.com>
2017-02-27 07:33:08 -0600

    Addressing 2/27/17 CSS Changes by RTM

--- userscript.js
+++ userscript.js
@@ -17,18 +17,18 @@ GM_addStyle('.b-f-m-n-o-p { background-color: #0060BF !important; color: #E0EBD5
 GM_addStyle('.b-f-m-n-o-Nj { background-color: #0060BF !important; color: #E0EBD5 !important; }');
 
 // Take the metallic pea out of the left column
-GM_addStyle('.Ej-Om-o .Ej-Om-wr { background-color: #0060BF !important; color: #0060BF !important; }'); //12/16/16
+GM_addStyle('.Ej-Om-o .Ej-Om-xr { background-color: #0060BF !important; color: #0060BF !important; }'); //2/27/17
 
 // Take the metallic pea out of the new tag in the Smart Add Bar - 11/3/16 verified
-GM_addStyle('.b-Dn-n-Ls-Ns-o { background-color: #E4EEF8 !important; color: #0060BF !important; }'); //2/23/17
-GM_addStyle('.b-Dn-n-Ls-Ns-o .b-Dn-n-Ls-Ns-i-fb.b-k-i-Pm { background-color: #E4EEF8 !important; color: #0060BF !important; }'); //2/23/17
+GM_addStyle('.b-En-n-Ls-Ns-o { background-color: #E4EEF8 !important; color: #0060BF !important; }'); //2/27/17
+GM_addStyle('.b-En-n-Ls-Ns-o .b-En-n-Ls-Ns-i-fb.b-k-i-Pm { background-color: #E4EEF8 !important; color: #0060BF !important; }'); //2/27/17
 
 // Take the metallic pea out of tag suggestion box
-GM_addStyle('.b-ln-wr { background-color: #0060BF !important; }'); //12/16/16
+GM_addStyle('.b-mn-xr { background-color: #0060BF !important; }'); //2/27/17
 
 /* Others */
-GM_addStyle('.b-c-d-e-f-Xr-o { background-color: #0060BF !important; }'); //12/16/16
+GM_addStyle('.b-c-d-e-f-Yr-o { background-color: #0060BF !important; }'); //2/27/17
 
 /* Get Rid of the Pro Nag in the lower left */
-GM_addStyle('.b-Ep-jo-Rj-Yn { display:none !important;}'); //12/16/16
-GM_addStyle('.b-Ep-Ej-Fp-Yn { height:90%; }');//12/16/16
+GM_addStyle('.b-Fp-ko-Rj-Zn { display:none !important;}'); //12/16/16
+GM_addStyle('.b-Fp-Ej-Gp-Zn { height:90%; }');//12/16/16
