51a52a1783f4ee27f81a829a53cdf3a8576eff57
themarkwilliams <themarkwilliams@users.noreply.github.com>
2017-12-27 21:15:34 -0600

    RTM CSS 2182 Changes - December 2017

--- userscript.js
+++ userscript.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name EnhanceRTM
-// @version 1.12
+// @version 1.13
 // @author Mark Williams
 // @description "Get rid of the metallic pea highlight color"
 // @match        https://www.rememberthemilk.com/*
@@ -10,25 +10,24 @@
 // Git: https://github.com/themarkwilliams/EnhanceRTM/blob/master/userscript.js
 
 // Set tag color in task list
-GM_addStyle('.b-db-Ym-o { background-color: #0060BF !important; }'); // 8/15/17
+GM_addStyle('.b-Z-Xm-Gb { background-color: #0060BF !important; }'); // 12/27/17
 
 // Set tag in right detail pane
-GM_addStyle('.b-f-m-n-o-p { background-color: #0060BF !important; color: #E0EBD5 !important; }');// 4/20/17
-GM_addStyle('.b-f-m-n-o-Nj { background-color: #0060BF !important; color: #E0EBD5 !important; }');// 4/20/17
+GM_addStyle('.b-f-Hb-om-Gb-Hn { background-color: #0060BF !important; color: #E0EBD5 !important; }');// 12/27/17
+GM_addStyle('.b-f-Hb-om-Gb-Lj { background-color: #0060BF !important; color: #E0EBD5 !important; }');// 12/27/17
 
 // Take the metallic pea out of the left column
-GM_addStyle('.Ej-Ym-o .Ej-Ym-Er { background-color: #0060BF !important; color: #0060BF !important; }'); // 8/15/17
+GM_addStyle('.Cj-Xm-Gb .Cj-Xm-Er { background-color: #0060BF !important; color: #0060BF !important; }'); // 12/27/17
 
 // Take the metallic pea out of the new tag in the Smart Add Bar
-GM_addStyle('.b-On-n-Rs-Ts-o { background-color: #E4EEF8 !important; color: #0060BF !important; }'); // 8/15/17
-GM_addStyle('.b-On-n-Rs-Ts-o .b-On-n-Rs-Ts-i-fb.b-k-i-Zm { background-color: #E4EEF8 !important; color: #0060BF !important; }'); // 8/15/17
+GM_addStyle('.b-On-om-Rs-Ts-Gb { background-color: #E4EEF8 !important; color: #0060BF !important; }'); // 12/27/17
+GM_addStyle('.b-Kb-Dj-lk-Bp-mk .b-f-Hb-om-Gb-Lj { background-color: #E4EEF8 !important; color: #0060BF !important; }'); // 12/27/17
 
 // Take the metallic pea out of tag suggestion box
-GM_addStyle('.b-wn-Er { background-color: #0060BF !important; }'); // 8/15/17
+GM_addStyle('.b-vn-Er { background-color: #0060BF !important; }'); // 12/27/17
 
 /* Others */
-GM_addStyle('.b-c-d-e-f-es-o { background-color: #0060BF !important; }'); // 4/20/17
+GM_addStyle('.b-c-d-e-f-es-Gb { background-color: #0060BF !important; }'); // 12/27/17
 
 /* Get Rid of the Pro Nag in the lower left */
-GM_addStyle('.b-Op-Ej-Pp-lo { top:0 !important; bottom:0 !important; height:100% !important; }'); // 8/15/17
-GM_addStyle('.b-wo-Rj { display:none !important; }'); // 8/15/17
+GM_addStyle('.b-Op-wo-Pj-lo { display:none !important; }'); // 12/27/17
