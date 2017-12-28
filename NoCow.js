2a6309eb1968e74d3edf6e50116eae415aa1df71
themarkwilliams <themarkwilliams@users.noreply.github.com>
2017-12-27 21:16:06 -0600

    RTM CSS 2182 Changes - December 2017

--- NoCow.js
+++ NoCow.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name RemoveRTMLogo
-// @version 1.4
+// @version 1.5
 // @author Mark Williams
 // @description "Get rid of the whimsical Cow Logo from Rememember the Milk"
 // @match        https://www.rememberthemilk.com/*
@@ -10,15 +10,15 @@
 // Git: https://github.com/themarkwilliams/EnhanceRTM/blob/master/logo.js
 
 // Hide the Logo
-GM_addStyle('.b-Cn-Do { display:none !important; }'); // 8/15/17
+GM_addStyle('.b-Bn-Do { display:none !important; }'); // 12/27/17
 
 // Adjust the height of the left navigation to compensate for the missing logo
-GM_addStyle('.b-Op-Ej-Pp-lo { top:0 !important; bottom:0 !important; height:100% !important; }'); // 8/15/17
+GM_addStyle('.b-Op-Cj-Pp-lo { top:0 !important; bottom:0 !important; height:100% !important; }'); // 12/27/17
 
 // Change the Favicon
 var favicon_link_html = document.querySelector("link[rel*='icon']") || document.createElement('link');
 favicon_link_html.rel = 'shortcut icon';
-favicon_link_html.href = 'http://marktastic.com/images/rtmfavicon.ico';
+favicon_link_html.href = 'https://marktastic.com/images/rtmfavicon.ico';
 favicon_link_html.type = 'image/x-icon';
 
 try { 
