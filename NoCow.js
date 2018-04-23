ad849ec7509bb3be459bd2da68bc4af81c2fd960
themarkwilliams <themarkwilliams@users.noreply.github.com>
2018-04-23 17:42:33 -0500

    Fixing RTM March 2018 CSS Changes

--- NoCow.js
+++ NoCow.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name RemoveRTMLogo
-// @version 1.5
+// @version 1.6
 // @author Mark Williams
 // @description "Get rid of the whimsical Cow Logo from Rememember the Milk"
 // @match        https://www.rememberthemilk.com/*
@@ -10,10 +10,10 @@
 // Git: https://github.com/themarkwilliams/EnhanceRTM/blob/master/logo.js
 
 // Hide the Logo
-GM_addStyle('.b-Bn-Do { display:none !important; }'); // 12/27/17
+GM_addStyle('.b-Dn-Jo { display:none !important; }'); // 4/23/18
 
 // Adjust the height of the left navigation to compensate for the missing logo
-GM_addStyle('.b-Op-Cj-Pp-lo { top:0 !important; bottom:0 !important; height:100% !important; }'); // 12/27/17
+GM_addStyle('.b-Up-Fj-Vp-ro { top:0 !important; bottom:0 !important; height:100% !important; }'); // 4/23/18
 
 // Change the Favicon
 var favicon_link_html = document.querySelector("link[rel*='icon']") || document.createElement('link');
