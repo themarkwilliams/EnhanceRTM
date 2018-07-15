e090ab5a168134d2c61f7e9c2f9f4981bf6f70e9
themarkwilliams <themarkwilliams@users.noreply.github.com>
2018-07-14 20:14:51 -0500

    Updating @name to standardize naming convention - Updating version number

--- userscript.js
+++ userscript.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Enhance RTM
-// @version 1.14
+// @version 1.15
 // @author Mark Williams
 // @description "Get rid of the metallic pea highlight color"
 // @match        https://www.rememberthemilk.com/*
